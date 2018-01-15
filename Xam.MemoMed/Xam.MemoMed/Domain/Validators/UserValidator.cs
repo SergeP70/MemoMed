71fc11944f77f91163ca3de01bd061ec6a45b904
Serge Pille <27204764+SergeP70@users.noreply.github.com>
2018-01-15 14:57:39 +0100

    Remove validationError when correct

--- Xam.MemoMed/Xam.MemoMed/Domain/Validators/UserValidator.cs
+++ Xam.MemoMed/Xam.MemoMed/Domain/Validators/UserValidator.cs
@@ -24,9 +24,9 @@ namespace Xam.MemoMed.Domain.Validators
 
             RuleFor(user => user.Phone)
                 .NotEmpty()
-                .WithMessage("Telefoon is verplicht")
-                .Matches(@"(?:(?:(\s*\(?([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2‌​-9][02-8]1|[2-9][02-8][02-9]))\)?\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9]‌​[02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})")
-                .WithMessage("Geen geldig telefoonnummer");
+                .WithMessage("Telefoon is verplicht");
+                //.Matches(@"(?:(?:(\s*\(?([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*)|([2-9]1[02-9]|[2‌​-9][02-8]1|[2-9][02-8][02-9]))\)?\s*(?:[.-]\s*)?)([2-9]1[02-9]|[2-9][02-9]1|[2-9]‌​[02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})")
+                //.WithMessage("Geen geldig telefoonnummer");
 
         }
     }
--- Xam.MemoMed/Xam.MemoMed/PageModels/ContactsPageModel.cs
+++ Xam.MemoMed/Xam.MemoMed/PageModels/ContactsPageModel.cs
@@ -13,29 +13,129 @@ namespace Xam.MemoMed.PageModels
         User contactPerson;
         private IValidator userValidator;
 
-
         public ContactsPageModel(IUsersService usersService)
         {
             this.usersService = usersService;
             userValidator = new UserValidator();
-            //get user 2
+            // for now: just get user 2
             contactPerson = usersService.GetUserById(2).Result;
-            //bind contact to the Page's ItemSource
-            // MedName = 0;    //Important! ensure the list is empty first to force refresh!
+        }
+
+
+        /// <summary>
+        /// This methods is called when the View is appearing
+        /// </summary>
+        protected override void ViewIsAppearing(object sender, System.EventArgs e)
+        {
+            base.ViewIsAppearing(sender, e);
+            // (enkel) bij opstart wordt deze methode 2x uitgevoerd
+            // CoreMethods.DisplayAlert("Page is appearing", "", "Ok");
+                        //bind contact to the Page's ItemSource
+            LoadContactsState();
+        }
+
+
+        /// <summary>
+        /// Refreshes the currentBucket (to edit) or initializes a new one (to add)
+        /// </summary>
+        /// <returns></returns>
+        //private async Task RefreshBucket()
+        //{
+        //    if (currentBucket != null)
+        //    {
+        //        PageTitle = currentBucket.Title;
+        //        currentBucket = await bucketService.GetBucketList(currentBucket.Id);
+        //    }
+        //    else
+        //    {
+        //        PageTitle = "New Bucket List";
+        //        currentBucket = new Bucket();
+        //        currentBucket.Id = Guid.NewGuid();
+        //        currentBucket.Owner = currentUser;
+        //        currentBucket.Items = new List<BucketItem>();
+        //    }
+        //    LoadBucketState();
+        //}
+
+        /// <summary>
+        /// Loads the contacts properties into the VM properties for display in UI
+        /// </summary>
+        private void LoadContactsState()
+        {
             ContactName = contactPerson.Name;
             ContactEmail = contactPerson.Email;
             ContactPhone = contactPerson.Phone;
         }
 
+
+        /// <summary>
+        /// Saves the VM properties back to the current contacts
+        /// </summary>
+        private void SaveContactsState()
+        {
+            contactPerson.Name = ContactName;
+            contactPerson.Email = ContactEmail;
+            contactPerson.Phone = ContactPhone;
+        }
+
+
         public Command SaveContactCommand
         {
             get
             {
-                return new Command(async() =>
+                return new Command(async () =>
                 {
-                    await CoreMethods.PushPageModel<TimeToTakePageModel>(null, true);
+                    SaveContactsState();
+                    if (Validate(contactPerson))
+                    {
+                        await CoreMethods.PushPageModel<TimeToTakePageModel>(null, true);
+
+                    }
                 });
             }
+        }
+
+        //public ICommand SaveBucketCommand => new Command(
+        //    async () => 
+        //    {
+        //        SaveBucketState();
+        //        if (Validate(currentBucket))
+        //        {
+        //            await bucketService.SaveBucketList(currentBucket);
+
+        //            MessagingCenter.Send(this,
+        //                Constants.MessageNames.BucketSaved, currentBucket);
+
+        //            await CoreMethods.PopPageModel(false, true);
+        //        }
+        //    });
+
+
+        /// <summary>
+        /// Validates the user using the validator
+        /// </summary>
+        /// <param name="user">The user to validate</param>
+        /// <returns></returns>
+        private bool Validate(User user)
+        {
+            var validationResult = userValidator.Validate(user);
+            //loop through error to identify properties
+            foreach (var error in validationResult.Errors)
+            {
+                if (error.PropertyName == nameof(user.Name))
+                {
+                    ContactNameError = error.ErrorMessage;
+                }
+                if (error.PropertyName == nameof(user.Email))
+                {
+                    ContactEmailError = error.ErrorMessage;
+                }
+                if (error.PropertyName == nameof(user.Phone))
+                {
+                    ContactPhoneError = error.ErrorMessage;
+                }
+            }
+            return validationResult.IsValid;
         }
 
 
@@ -50,6 +150,23 @@ namespace Xam.MemoMed.PageModels
             }
         }
 
+        private string contactNameError;
+        public string ContactNameError
+        {
+            get { return contactNameError; }
+            set
+            {
+                contactNameError = value;
+                RaisePropertyChanged(nameof(ContactNameError));
+                RaisePropertyChanged(nameof(ContactNameErrorVisible));
+            }
+        }
+
+        public bool ContactNameErrorVisible
+        {
+            get { return !string.IsNullOrWhiteSpace(ContactNameError); }
+        }
+
         private string contactEmail;
         public string ContactEmail
         {
@@ -59,6 +176,22 @@ namespace Xam.MemoMed.PageModels
                 contactEmail = value;
                 RaisePropertyChanged(nameof(ContactEmail));
             }
+        }
+        private string contactEmailError;
+        public string ContactEmailError
+        {
+            get { return contactEmailError; }
+            set
+            {
+                contactEmailError = value;
+                RaisePropertyChanged(nameof(ContactEmailError));
+                RaisePropertyChanged(nameof(ContactEmailErrorVisible));
+            }
+        }
+
+        public bool ContactEmailErrorVisible
+        {
+            get { return !string.IsNullOrWhiteSpace(ContactEmailError); }
         }
 
         private string contactPhone;
@@ -71,5 +204,23 @@ namespace Xam.MemoMed.PageModels
                 RaisePropertyChanged(nameof(ContactPhone));
             }
         }
+
+        private string contactPhoneError;
+        public string ContactPhoneError
+        {
+            get { return contactPhoneError; }
+            set
+            {
+                contactPhoneError = value;
+                RaisePropertyChanged(nameof(ContactPhoneError));
+                RaisePropertyChanged(nameof(ContactPhoneErrorVisible));
+            }
+        }
+
+        public bool ContactPhoneErrorVisible
+        {
+            get { return !string.IsNullOrWhiteSpace(ContactPhoneError); }
+        }
+
     }
 }
